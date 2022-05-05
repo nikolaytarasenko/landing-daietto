32f1f4843495a552f5b68b8f6622818dc8498a58
Nikolay Tarasenko <kolyatarasenko@ukr.net>
2022-05-05 12:20:20 +0300

    animation was added

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 const toggleMobileNavigation = require('./module/toggleMobileNavigation.js');
 const hideMobileNavigationOnDesktop = require('./module/hideMobileNavigationOnDesktop.js');
 const initStickyHeader = require('./module/initStickyHeader.js');
+const WOW = require('wowjs');
 
 const init = () => {
     const hamburger = document.querySelector('.hamburger');
@@ -8,6 +9,10 @@ const init = () => {
     hamburger.addEventListener('click', toggleMobileNavigation);
     window.addEventListener('resize', hideMobileNavigationOnDesktop);
     window.addEventListener('scroll', initStickyHeader);
+
+    new WOW.WOW({
+        live: false
+    }).init();
 }
 
 window.addEventListener('load', init);
